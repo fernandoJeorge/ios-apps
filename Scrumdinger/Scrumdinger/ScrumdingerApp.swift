ee045032bc94c271775669e983ab400ef7d90295
fernandoJeorge <fernandoespiry.99@gmail.com>
2025-05-13 22:56:26 -0400

    add/update Xcode project files

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Fernando Cardona on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
