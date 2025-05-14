e5649b108eb8291c1edc2a6207780c025c946269
fernandoJeorge <fernandoespiry.99@gmail.com>
2025-05-14 19:36:32 -0400

    added the card view, display data in a list, navigation hierarchy and working in data flow between views

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -1,27 +1,28 @@
-//
-//  ScrumsView.swift
-//  Scrumdinger
-//
-//  Created by Fernando Cardona on 5/14/25.
-//
-
 import SwiftUI
+
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
-    
-    
+  
     var body: some View {
-        NavigationStack{
+        NavigationStack {
             List(scrums) { scrum in
-                NavigationLink(destination: Text(scrum.title)){
+                NavigationLink(destination: DetailView(scrum: scrum)) {
                     CardView(scrum: scrum)
-                        .listRowBackground(scrum.theme.mainColor)
                 }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
             }
         }
     }
 }
+
 
 #Preview {
     ScrumsView(scrums: DailyScrum.sampleData)
